ff63f7dded984cc6f387f16b20b0a2649810354a
jeps <petteri.stenius@ubisecure.com>
2019-01-18 13:28:40 +0200

    updates

--- docs/worker.js
+++ docs/worker.js
@@ -1,4 +1,9 @@
 /*
+ * subscription.js
+ */
+self.importScripts("subscription.js");
+
+/*
  * https://w3c.github.io/ServiceWorker/#service-worker-global-scope-install-event
  */
 self.addEventListener("install", event => {
@@ -42,30 +47,64 @@ self.addEventListener("notificationclick", event => {
 	console.log("notificationclick notification.data=" + JSON.stringify(event.notification.data));
     event.notification.close(); 
     var data = event.notification.data;
-    if(data && data.client) {        
-        console.log("postMessage");
-        var match = self.clients.matchAll()
-            .then(clients => {
-                console.log("matchAll.then()");
-                var list = [];
-                clients.forEach(client => {
-                    console.log("forEach " + client);
-                    client.postMessage("notificationclick -> postMessage");
-                    list.push(client.focus());
+    if(data && data.fromClient) {        
+        if(data.openWindow) {
+            console.log("openWindow");
+            event.waitUntil(self.clients.openWindow("/push-demo/worker.html").then(w => w.postMessage("jeejee")));
+        } else if(data.focus) {
+            console.log("postMessage");
+            var match = self.clients.matchAll()
+                .then(clients => {
+                    console.log("matchAll.then()");
+                    var list = [];
+                    clients.forEach(client => {
+                        console.log("forEach " + client);
+                        var msg = {
+                            action: event.action,
+                            data: event.notification.data
+                        };
+                        client.postMessage(JSON.stringify(msg));
+                        list.push(client.focus());
+                    });
+                    return Promise.all(list);
                 });
-                return Promise.all(list);
-            });
-        event.waitUntil(match);
+            event.waitUntil(match);
+        } else if(data.fetch) {
+            console.log("fetch");
+            var f = fetch("/push-demo/base64url.js").then(response => response.ok ? Promise.resolve() : Promise.reject(response));
+            event.waitUntil(f);
+        }
 /*        
         self.clients.openWindow(location.origin + "/push-demo/index.html")
             .then(window => 
 */            
-    } else if(data && data.subid && data.push_id) {
-        var uri = location.origin + "/push-demo/authorize.html#" + data.subid + "/" + data.push_id;
-        console.log("openWindow " + uri);
-        event.waitUntil(self.clients.openWindow(uri));
+    } else if(data && data.message) {
+        var message = data.message;
+        var uri;
+        var reject;
+        if(event.action == "reject") {
+            reject = reject_push_request(message.subid, message.push_id);
+            uri = location.origin + "/push-demo/subscription.html#" + encode_location(message.subid);
+        } else {
+            reject = Promise.resolve();
+            uri = location.origin + "/push-demo/authorize.html#" + encode_location(message.subid, message.push_id);
+        }
+        var navigate = self.clients.matchAll()
+            .then(clients => {
+                var list = [];
+                clients.forEach(client => {
+                    console.log("navigate " + uri);
+                    list.push(client.navigate(uri).then(client => client.focus()));
+                });
+                if(list.length == 0) {
+                    console.log("openWindow " + uri);
+                    list.push(self.clients.openWindow(uri));
+                }
+                return Promise.all(list);
+            });
+        event.waitUntil(reject.then(() => navigate));
     }
-});
+}, false);
 
 /*
  * https://w3c.github.io/push-api/#the-push-event
@@ -73,22 +112,28 @@ self.addEventListener("notificationclick", event => {
 self.addEventListener("push", event => {
 	console.log("push " + event);
 	console.log("push.data " + event.data.text());
+    var message = event.data.json();
     if(false) {
-        var data = event.data.json();
-        var uri = location.origin + "/push-demo/authorize.html#" + data.subid + "/" + data.push_id;
+        var uri = location.origin + "/push-demo/authorize.html#" + message.subid + "/" + message.push_id;
         console.log("openWindow " + uri);
         event.waitUntil(clients.openWindow(uri));
     } else {
-        var n = self.registration.showNotification("Push", {
-            body:event.data.text(),
-            tag:"push-demo",
-            requireInteraction:true,
-            data:event.data.json(),
-            actions:[
-                {action:"Yes",title:"Yes"},
-                {action:"No",title:"No"}
-            ]
-        });
+        var title = "Sign-in request";
+        var options = {
+            body: "Request " + message.binding_message + " from " + message.client_name + "\r\n" + message.login_hint,
+            icon: "/push-demo/256.png",
+            badge: "/push-demo/256bw.png",
+            tag: location.origin,
+            actions: [
+                { action: "show", title: "Show" },
+                { action: "reject", title: "Reject" }
+            ],
+            data: {
+                fromClient: false,
+                message: message
+            }
+        };
+        var n = self.registration.showNotification(title, options);
         event.waitUntil(n);
     }
 });
